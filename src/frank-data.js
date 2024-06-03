f6380c743cfdd5e1794c4730697d9c407b9afa1b
Finn Welsford-Ackroyd <finnwa24@gmail.com>
2024-06-03 23:50:56 +1200

    Get all Frank electricity data

---
+++ src/frank-data.js
@@ -0,0 +1,66 @@
+import { readFileSync } from "fs";
+import { Temporal } from "temporal-polyfill";
+import { writeJson } from "./plans/utils.js";
+
+await getFrankHourlyElectricityData();
+
+async function getFrankHourlyElectricityData() {
+  const auth = readFileSync("./data/auth.txt", { encoding: "utf8" });
+  const rangeStart = Temporal.PlainDate.from("2024-01-13");
+  const rangeEnd = Temporal.PlainDate.from("2024-06-01");
+  let startDate = rangeStart;
+
+  while (startDate.dayOfYear < rangeEnd.dayOfYear) {
+    const endDate = startDate.add({ days: 5 });
+    const response = await requestUsage(
+      {
+        startDate: startDate.toString(),
+        endDate: endDate.toString(),
+        intervalType: "HOURLY",
+      },
+      auth
+    );
+    writeJson(
+      response,
+      `./data/electricity/${startDate.toString()}_${endDate.toString()}.json`
+    );
+    startDate = endDate;
+  }
+}
+
+/**
+ * @param {{ startDate: string; endDate: string;  intervalType: string; }} body
+ * @param {string} authorization
+ * @returns {Promise<import("./parse-data.js").FrankElectricityUsageFile>}
+ */
+async function requestUsage(body, authorization) {
+  const bodyStr = JSON.stringify(body);
+  console.log("Sending request: " + bodyStr);
+  const response = await fetch(
+    "https://web-api.energyonline.co.nz/v2/private/electricity/site-usage",
+    {
+      credentials: "include",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0",
+        Accept: "application/json",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/json",
+        Authorization: authorization,
+        "Brand-Id": "GEOL",
+        "Cache-Control": "no-cache,no-store,must-revalidate,max-age=0",
+        Pragma: "no-cache",
+        Expires: "0",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site",
+        Priority: "u=1",
+      },
+      referrer: "https://myaccount.frankenergy.co.nz/",
+      body: bodyStr,
+      method: "POST",
+      mode: "cors",
+    }
+  );
+  return response.json();
+}
